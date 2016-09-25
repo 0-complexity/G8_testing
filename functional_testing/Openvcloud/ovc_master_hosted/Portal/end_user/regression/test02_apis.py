c78d75cfd43dfe5587a59b0eaa4901d2a9dcb7cd
Mahmoud Ali <simplymahmoud@gmail.com>
2016-09-25 14:08:18 +0200

    A_Test_2.1.0b: Testcase to check whether AccountId is working as a filter when trying to list images task #299

--- functional_testing/Openvcloud/ovc_master_hosted/Portal/end_user/regression/test02_apis.py
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/end_user/regression/test02_apis.py
@@ -1,6 +1,36 @@
+from functional_testing.Openvcloud.ovc_master_hosted.Portal.utils.utils import BaseTest
+import unittest
 
-__author__="mahmoud"
-__date__ ="$May 3, 2016 8:17:56 AM$"
 
-if __name__ == "__main__":
-    pass
+class APIsTests(BaseTest):
+    def __init__(self, *args, **kwargs):
+        super(APIsTests, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        super(APIsTests, self).setUp()
+        self.login()
+        self.click("machine_api_button")
+
+    @unittest.skip("bug: https://github.com/0-complexity/openvcloud/issues/178")
+    def test01_list_images_using_account(self):
+        """ PRTL-025
+        *Test case for check list images using accountId is working.*
+
+        **Test Scenario:**
+
+        #. check list images without accountId, should succeed
+        #. check list images with valid accountId, should succeed
+        #. check list images with invalid accountId, should succeed
+        """
+        self.lg('%s STARTED' % self._testID)
+        self.click("cloudapi_images_show")
+        self.click("cloudapi_images_list_btn")
+        self.click("cloudapi_images_list_tryit")
+        self.assertNotEqual(self.get_text("cloudapi_images_list_body"), "[]")
+        self.set_text('cloudapi_images_list_accountid', 2)
+        self.click("cloudapi_images_list_tryit")
+        self.assertNotEqual(self.get_text("cloudapi_images_list_body"), "[]")
+        self.set_text('cloudapi_images_list_accountid', 100000)
+        self.click("cloudapi_images_list_tryit")
+        self.assertEqual(self.get_text("cloudapi_images_list_body"), "[]")
+        self.lg('%s ENDED' % self._testID)
