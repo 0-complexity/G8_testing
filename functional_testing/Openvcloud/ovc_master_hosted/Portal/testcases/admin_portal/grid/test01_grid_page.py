930c56243da28bd07b6114e59807d25ad27939e6
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2017-02-22 12:02:37 +0200

    grid testcases scenario

---
+++ functional_testing/Openvcloud/ovc_master_hosted/Portal/testcases/admin_portal/grid/test01_grid_page.py
@@ -0,0 +1,18 @@
+#class GridTests(Framework):
+    #def setUp(self):
+        #super(GridTests, self).setUp()
+        #self.Login.Login()
+
+    #def test001_grid_page(self):
+    #    """
+    #   *Test case for checking error condition page in the admin portal*
+    #    **Test Scenario:**
+    #
+    #    #. get grid page
+    #    #. check that "Grid Portal" in head of page side of administration
+    #    #. check that all elements on grid page exist(headers,Events counter,Running VMs counter,Grid Nodes,Events Dashboard,last check,alert)
+    #    #. check that you can get grid nodes page from grid page
+    #    #. check that you can get failed jobs page from grid page
+    #    #. check that you can get status overview from grid page
+    #    #. check that all nodes running well from alert element in grid page
+    #    """
