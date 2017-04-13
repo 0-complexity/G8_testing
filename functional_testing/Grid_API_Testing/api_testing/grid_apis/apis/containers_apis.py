c9d2e9014b6f4fb7b2d5676911b8462b6a724b04
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2017-04-13 15:27:55 +0200

    complete container apis

--- functional_testing/Grid_API_Testing/api_testing/grid_apis/apis/containers_apis.py
+++ functional_testing/Grid_API_Testing/api_testing/grid_apis/apis/containers_apis.py
@@ -16,62 +16,126 @@ class ContainersAPI(GridAPIBase):
         return self.request_api(method=method,
                                 api=api, body=body)
 
-    def delete_containers_containerid(self, node_id, containerid):
+    def delete_containers_containerid(self, node_id, container_id):
         method = 'delete'
-        api = ['nodes', node_id, 'containers', containerid]
+        api = ['nodes', node_id, 'containers', container_id]
         return self.request_api(method=method,
                                 api=api)
 
-    def get_containers_containerid(self, node_id, containerid):
+    def get_containers_containerid(self, node_id, container_id):
         method = 'get'
-        api = ['nodes', node_id, 'containers', containerid]
+        api = ['nodes', node_id, 'containers', container_id]
         return self.request_api(method=method,
                                 api=api)
 
-    def post_containers_containerid_start(self, node_id, containerid):
+    def post_containers_containerid_start(self, node_id, container_id):
         method = 'post'
-        api = ['nodes', node_id, 'containers', containerid, 'start']
+        api = ['nodes', node_id, 'containers', container_id, 'start']
         return self.request_api(method=method,
                                 api=api)
-    def post_containers_containerid_stop(self, node_id, containerid):
+    def post_containers_containerid_stop(self, node_id, container_id):
         method = 'post'
-        api = ['nodes', node_id, 'containers', containerid, 'stop']
+        api = ['nodes', node_id, 'containers', container_id, 'stop']
         return self.request_api(method=method,
                                 api=api)
 
-    def post_containers_containerid_filesystem(self, node_id, containerid, body):
+    def post_containers_containerid_filesystem(self, node_id, container_id, body):
         method = 'post'
-        api = ['nodes', node_id, 'containers', containerid, 'filesystem']
+        api = ['nodes', node_id, 'containers', container_id, 'filesystem']
         return self.request_api(method=method,
                                 api=api, body=body)
 
 
-    def get_containers_containerid_filesystem(self, node_id, containerid):
+    def get_containers_containerid_filesystem(self, node_id, container_id):
         method = 'get'
-        api = ['nodes', node_id, 'containers', containerid, 'filesystem']
+        api = ['nodes', node_id, 'containers', container_id, 'filesystem']
         return self.request_api(method=method,
                                 api=api)
 
-    def delete_containers_containerid_filesystem(self, node_id, containerid):
+    def delete_containers_containerid_filesystem(self, node_id, container_id):
         method = 'delete'
-        api = ['nodes', node_id, 'containers', containerid, 'filesystem']
+        api = ['nodes', node_id, 'containers', container_id, 'filesystem']
         return self.request_api(method=method,
                                 api=api)
 
-    def get_containers_containerid_jobs(self, node_id, containerid):
+    def get_containers_containerid_jobs(self, node_id, container_id):
         method = 'get'
-        api = ['nodes', node_id, 'containers', containerid, 'jobs']
+        api = ['nodes', node_id, 'containers', container_id, 'jobs']
         return self.request_api(method=method,
                                 api=api)
 
-    def post_containers_containerid_jobs(self, node_id, containerid):
+    def post_containers_containerid_jobs(self, node_id, container_id):
         method = 'post'
-        api = ['nodes', node_id, 'containers', containerid, 'jobs']
+        api = ['nodes', node_id, 'containers', container_id, 'jobs']
         return self.request_api(method=method,
                                 api=api)
 
-    def post_containers_containerid_jobs(self, node_id, containerid):
+    def post_containers_containerid_jobs(self, node_id, container_id):
         method = 'post'
-        api = ['nodes', node_id, 'containers', containerid, 'jobs']
+        api = ['nodes', node_id, 'containers', container_id, 'jobs']
+        return self.request_api(method=method,
+                                api=api)
+
+    def get_containers_containerid_jobs_jobid(self, node_id, container_id, job_id):
+        method = 'get'
+        api = ['nodes', node_id, 'containers', container_id, 'jobs', job_id]
+        return self.request_api(method=method,
+                                api=api)
+
+    def post_containers_containerid_jobs_jobid(self, node_id, container_id, job_id ,body):
+        method = 'post'
+        api = ['nodes', node_id, 'containers', container_id, 'jobs', job_id]
+        return self.request_api(method=method,
+                                api=api, body=body)
+
+    def delete_containers_containerid_jobs_jobid(self, node_id, container_id, job_id):
+        method = 'delete'
+        api = ['nodes', node_id, 'containers', container_id, 'jobs', job_id]
+        return self.request_api(method=method,
+                                api=api)
+    def post_containers_containerid_ping(self, node_id, container_id):
+        method = 'post'
+        api = ['nodes', node_id, 'containers', container_id, 'ping']
+        return self.request_api(method=method,
+                                api=api)
+    def get_containers_containerid_state(self, node_id, container_id):
+        method = 'get'
+        api = ['nodes', node_id, 'containers', container_id, 'state']
+        return self.request_api(method=method,
+                                api=api)
+
+    def get_containers_containerid_info(self, node_id, container_id):
+        method = 'get'
+        api = ['nodes', node_id, 'containers', container_id, 'info']
+        return self.request_api(method=method,
+                                api=api)
+
+    def get_containers_containerid_processes(self, node_id, container_id):
+        method = 'get'
+        api = ['nodes', node_id, 'containers', container_id, 'processes']
+        return self.request_api(method=method,
+                                api=api)
+
+    def post_containers_containerid_processes(self, node_id, container_id, body):
+        method = 'post'
+        api = ['nodes', node_id, 'containers', container_id, 'processes']
+        return self.request_api(method=method,
+                                api=api, body=body)
+
+    def get_containers_containerid_processes_processid(self, node_id, container_id, processid):
+        method = 'get'
+        api = ['nodes', node_id, 'containers', container_id, 'processes',processid]
+        return self.request_api(method=method,
+                                api=api)
+
+    def post_containers_containerid_processes_processid(self, node_id, container_id, processid,body):
+        method = 'post'
+        api = ['nodes', node_id, 'containers', container_id, 'processes',processid]
+        return self.request_api(method=method,
+                                api=api, body=body)
+
+    def delete_containers_containerid_processes_processid(self, node_id, container_id, processid):
+        method = 'delete'
+        api = ['nodes', node_id, 'containers', container_id, 'processes',processid]
         return self.request_api(method=method,
                                 api=api)
