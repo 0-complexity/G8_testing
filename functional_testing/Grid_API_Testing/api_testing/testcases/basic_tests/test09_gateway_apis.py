760d1702ae2b4e73b8e8aa2472858e73b7257fe1
islamTaha12 <tahai@codescalers.com>
2017-06-13 17:05:07 +0200

    add basic creation test cases

--- functional_testing/Grid_API_Testing/api_testing/testcases/basic_tests/test09_gateway_apis.py
+++ functional_testing/Grid_API_Testing/api_testing/testcases/basic_tests/test09_gateway_apis.py
@@ -1,5 +1,3 @@
-from random import randint
-import unittest, time
 from api_testing.testcases.testcases_base import TestcasesBase
 from api_testing.grid_apis.pyclient.bridges_apis import BridgesAPI
 from api_testing.grid_apis.pyclient.containers_apis import ContainersAPI
@@ -18,39 +16,36 @@ class TestGatewayAPI(TestcasesBase):
         super(TestGatewayAPI, self).setUp()
         self.nodeid = self.get_random_node()
         self.lg.info('Get random nodeid : %s' % str(self.nodeid))
-        pyclient_ip = [x['ip'] for x in self.nodes if x['id'] == self.nodeid]
-        self.assertNotEqual(pyclient_ip, [])
-        self.pyclient = Client(pyclient_ip[0])
+        core0_ip = [x['ip'] for x in self.nodes if x['id'] == self.nodeid]
+        self.assertNotEqual(core0_ip, [])
+        self.core0_client = Client(core0_ip[0])
 
-        self.gwname = self.random_string()
-        self.gwdomain = self.random_string()
+        self.gw_name = self.random_string()
+        self.gw_domain = self.random_string()
 
         self.body = {
-            "name":self.gwname,
-            "domain":self.gwdomain,
-            "nics":[
+            "name": self.gw_name,
+            "domain": self.gw_domain,
+            "nics": [
                 {
-                    'type':'default',
-                    'name':'internet'
+                    'type': 'default',
+                    'name': 'internet'
                 }
             ],
-            "portforwards":[],
-            "httpproxies":[]
+            "portforwards": [],
+            "httpproxies": []
         }
 
-        self.pyclient.create_ovs_container()
+        self.core0_client.create_ovs_container()
         response = self.gateways_apis.post_nodes_gateway(self.nodeid, self.body)
         self.assertEqual(response.status_code, 201)
-
 
     def tearDown(self):
         self.lg.info('Delete all node {} gateways'.format(self.nodeid))
         response = self.gateways_apis.list_nodes_gateways(self.nodeid)
         for gw in response.json():
             self.gateways_apis.delete_nodes_gateway(self.nodeid, gw['name'])
-
         super(TestGatewayAPI, self).tearDown()
-
 
     def test001_list_nodes_gateways(self):
         """ GAT-082
@@ -63,8 +58,7 @@ class TestGatewayAPI(TestcasesBase):
         self.lg.info('List node (N0) gateways, (GW0) should be listed')
         response = self.gateways_apis.list_nodes_gateways(self.nodeid)
         self.assertEqual(response.status_code, 200)
-        self.assertIn(self.gwname, [x['name'] for x in response.json()])
-
+        self.assertIn(self.gw_name, [x['name'] for x in response.json()])
 
     def test002_get_gateway_info(self):
         """ GAT-083
@@ -74,9 +68,8 @@ class TestGatewayAPI(TestcasesBase):
         #. Create gateway (GW0) on node (N0), should succeed.
         #. Get gateway (GW0) info, should succeed.
         """
-        response = self.gateways_apis.get_nodes_gateway(self.nodeid, self.gwname)
+        response = self.gateways_apis.get_nodes_gateway(self.nodeid, self.gw_name)
         self.assertEqual(response.status_code, 200)
-
 
     def test003_delete_gateway(self):
         """ GAT-084
@@ -89,31 +82,161 @@ class TestGatewayAPI(TestcasesBase):
         """
 
         self.lg.info('Delete gateway (GW0), should succeed')
-        response = self.gateways_apis.delete_nodes_gateway(self.nodeid, self.gwname)
+        response = self.gateways_apis.delete_nodes_gateway(self.nodeid, self.gw_name)
         self.assertEqual(response.status_code, 204)
 
         self.lg.info('List node (N0) gateways, (GW0) should not be listed')
         response = self.gateways_apis.list_nodes_gateways(self.nodeid)
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn(self.gwname, [x['name'] for x in response.json()])
+        self.assertNotIn(self.gw_name, [x['name'] for x in response.json()])
 
-
-    def test004_create_gateway(self):
+    def test004_create_gateway_with_vlan_vlan_container(self):
         """ GAT-085
         **Test Scenario:**
 
         #. Get random node (N0), should succeed.
-        #. Create gateway (GW0) on node (N0), should succeed.
+        #. Create gateway with vlan and vlan as nics on node (N0), should succeed.
+        #. Bind a new container to vlan(1).
+        #. Bind a new container to vlan(2).
+        #. Make sure that those two containers can ping each others.
         """
         pass
 
-
-    def test005_update_gateway_info(self):
+    def test005_create_gateway_with_vxlan_vxlan_container(self):
         """ GAT-086
         **Test Scenario:**
 
         #. Get random node (N0), should succeed.
-        #. Create gateway (GW0) on node (N0), should succeed.
-        #. Update gateway (GW0), should succeed.
+        #. Create gateway with vxlan and vxlan as nics on node (N0), should succeed.
+        #. Bind a new container to vxlan(1).
+        #. Bind a new container to vxlan(2).
+        #. Make sure that those two containers can ping each others.
         """
         pass
+
+    def test006_create_gateway_with_vlan_vlan_vm(self):
+        """ GAT-085
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with vlan and vlan as nics on node (N0), should succeed.
+        #. Bind a new vm to vlan(1).
+        #. Bind a new vm to vlan(2).
+        #. Make sure that those two containers can ping each others.
+        """
+        pass
+
+    def test007_create_gateway_with_vxlan_vxlan_vm(self):
+        """ GAT-086
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with vxlan and vxlan as nics on node (N0), should succeed.
+        #. Bind a new vm to vxlan(1).
+        #. Bind a new vm to vxlan(2).
+        #. Make sure that those two containers can ping each others.
+        """
+        pass
+
+    def test008_create_gateway_with_bridge_vlan_container(self):
+        """ GAT-087
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Bind a new container to vlan(1).
+        #. Verify that this container has public access.
+        """
+        pass
+
+    def test009_create_gateway_with_bridge_vlan_container(self):
+        """ GAT-088
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Bind a new container to vlan(1).
+        #. Verify that this container has public access.
+        """
+        pass
+
+    def test010_create_gateway_with_bridge_vxlan_container(self):
+        """ GAT-089
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Bind a new container to vxlan(1).
+        #. Verify that this container has public access.
+        """
+        pass
+
+    def test011_create_gateway_with_bridge_vlan_vm(self):
+        """ GAT-090
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Bind a new vm to vlan(1).
+        #. Verify that this container has public access.
+        """
+        pass
+
+    def test012_create_gateway_with_bridge_vxlan_vm(self):
+        """ GAT-091
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Bind a new vm to vxlan(1).
+        #. Verify that this vm has public access.
+        """
+        pass
+
+    def test013_create_gateway_dhcpserver(self):
+        """ GAT-092
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics on node (N0), should succeed.
+        #. Specify a dhcpserver for container and vm in this GW
+        #. Create a container and vm to match the dhcpserver specs
+        #. Verify that container and vm ips are matching with the dhcpserver specs.
+        """
+        pass
+
+    def test014_create_gateway_httpproxy(self):
+        """ GAT-091
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics and httpproxy with two containers on node (N0), should succeed.
+        #. Create two containers to for test the httpproxy's configuration
+        #. Verify that the httprxoy's configuration is working right
+        """
+        pass
+
+    def test015_create_gateway_portforwards(self):
+        """ GAT-091
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create gateway with bridge and vlan as nics should succeed.
+        #. Set a portforward form srcip:80 to destination:80
+        #. Create one container as a destination host
+        #. Start any service in this container
+        #. Using core0_client try to request this service and make sure that u can reach the container
+        """
+        pass
+
+
+    def test016_create_two_gateways_zerotierbridge(self):
+        """ GAT-091
+        **Test Scenario:**
+
+        #. Get random node (N0), should succeed.
+        #. Create two containers and link them with zerotier bridge.
+        #. Verify that each containers' hosts can reach each others.
+        """
+        pass
+    
