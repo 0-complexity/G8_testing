52c8792ba43b4b3e468eb69934f6f25e4472ce01
ahmedelsayed-93m <elsayeda@greenitglobe.com>
2017-06-19 12:23:18 +0200

    fix jwt

--- functional_testing/Grid_API_Testing/api_testing/grid_apis/orchestrator_base.py
+++ functional_testing/Grid_API_Testing/api_testing/grid_apis/orchestrator_base.py
@@ -1,4 +1,5 @@
 from zeroos.orchestrator import client as apiclient
+from zeroos.orchestrator.client import oauth2_client_itsyouonline
 from testconfig import config
 
 class GridPyclientBase(object):
@@ -7,12 +8,11 @@ class GridPyclientBase(object):
         self.api_base_url = self.config['api_base_url']
 
         # Get JWT
-        client_id = config['client_id']
-        client_secret = config['client_secret']
-        user_of_organization = config['user_of_organization']
-        cls = apiclient.oauth2_client_itsyouonline.Oauth2ClientItsyouonline()
-        response = cls.get_access_token(client_id, client_secret, scope=['organization:memberOf:%s' % user_of_organization],
-                                        audience=[])
+        client_id = self.config['client_id']
+        client_secret = self.config['client_secret']
+        user_of_organization = self.config['user_of_organization']
+        cls = oauth2_client_itsyouonline.Oauth2ClientItsyouonline()
+        response = cls.get_access_token(client_id, client_secret, scopes=['user:memberof:%s' % user_of_organization], audiences=[])
         JWT = response.token
 
         # Create client object
