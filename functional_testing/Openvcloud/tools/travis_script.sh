35577050ccd77410a76bd1fdb20c2a6e137eb619
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-11-15 15:25:00 +0200

    debug

--- functional_testing/Openvcloud/tools/travis_script.sh
+++ functional_testing/Openvcloud/tools/travis_script.sh
@@ -11,6 +11,8 @@ if [[ ${action} == "before" ]]; then
 
     sleep 30
 
+    ping -w5 ${ctrl_ipaddress}
+
     echo "[+] Cloning G8_testing repo : ${ctrl_ipaddress}"
     cmd="cd /tmp; rm -rf G8_testing; git clone -b ${TRAVIS_BRANCH} https://github.com/0-complexity/G8_testing"
     sshpass -p ${ctrl_password} ssh -t ${ctrl_user}@${ctrl_ipaddress} "pwd"
