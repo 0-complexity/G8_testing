240b2a4ebb61f1d29c91fd2e65d95245e79978dc
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-11-15 16:30:57 +0200

    clean

--- functional_testing/Openvcloud/tools/travis_script.sh
+++ functional_testing/Openvcloud/tools/travis_script.sh
@@ -15,17 +15,17 @@ if [[ ${action} == "before" ]]; then
 
     echo "[+] Cloning G8_testing repo"
     cmd="cd /tmp; rm -rf G8_testing; git clone -b ${TRAVIS_BRANCH} https://github.com/0-complexity/G8_testing"
-    sshpass -p "${ctrl_password}" ssh -t ${ctrl_user}@${ctrl_ipaddress} "${cmd}" > /dev/null
+    sshpass -p "${ctrl_password}" ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "${cmd}" > /dev/null
 
 elif [[ ${action} == "test" ]]; then
 
     echo "[+] Executing testsuite from path : ${testsuite_dir}"
     cmd="cd /tmp/G8_testing/functional_testing/Openvcloud; nosetests -s -v ${testsuite_dir} --tc-file config.ini --tc=main.environment:${environment}"
-    sshpass -p ${ctrl_password} ssh -t ${ctrl_user}@${ctrl_ipaddress} "${cmd}"
+    sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "${cmd}"
 
 elif [[ ${action} == "after" ]]; then
 
     echo "[+] Removing G8_testing repo"
-    sshpass -p ${ctrl_password} ssh -t ${ctrl_user}@${ctrl_ipaddress} "rm -rf /tmp/G8_testing"
+    sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "rm -rf /tmp/G8_testing"
 
 fi
