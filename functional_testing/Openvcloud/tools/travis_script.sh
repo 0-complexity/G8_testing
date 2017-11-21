8300f8aa21651d2598fed31d43182b061740ad87
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-11-21 13:50:52 +0200

    add jobs

--- functional_testing/Openvcloud/tools/travis_script.sh
+++ functional_testing/Openvcloud/tools/travis_script.sh
@@ -2,9 +2,14 @@ action=$1
 
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]; then
 
+    testsuite_repo_path="/tmp/travic-buid-${TRAVIS_BUILD_NUMBER}-${TRAVIS_JOB_NUMBER}"
+    python_path="export PYTHONPATH=/opt/jumpscale7/lib:/opt/jumpscale7/lib/lib-dynload/:/opt/jumpscale7/bin:/opt/jumpscale7/lib/python.zip:/opt/jumpscale7/lib/plat-x86_64-linux-gnu"
+
     if [[ ${action} == "before" ]]; then
 
-        ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+        echo "[+] Installing travis machine requirements"
+        sudo apt install git sshpass -y
+        curl -s https://install.zerotier.com/ | sudo bash
 
         echo "[+] Joining zerotier network : ${zerotier_network}"
         sudo zerotier-cli join ${zerotier_network}; sleep 10
@@ -16,24 +21,23 @@ if [ "$TRAVIS_EVENT_TYPE" == "cron" ] || [ "$TRAVIS_EVENT_TYPE" == "api" ]; then
         sleep 60
 
         echo "[+] Cloning G8_testing repo"
-        cmd="cd /tmp; rm -rf G8_testing; git clone -b ${TRAVIS_BRANCH} https://github.com/0-complexity/G8_testing"
+        cmd="mkdir ${testsuite_repo_path}; cd ${testsuite_repo_path}; rm -rf G8_testing; git clone -b ${TRAVIS_BRANCH} https://github.com/0-complexity/G8_testing"
         sshpass -p "${ctrl_password}" ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "${cmd}"
 
         echo "[+] Install requirements"
-        cmd="cd /tmp/G8_testing; pip install -r requirements.txt"
+        cmd="cd ${testsuite_repo_path}/G8_testing; pip install -r requirements.txt"
         sshpass -p "${ctrl_password}" ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "${cmd}"
 
     elif [[ ${action} == "test" ]]; then
 
-        python_path="export PYTHONPATH=/opt/jumpscale7/lib:/opt/jumpscale7/lib/lib-dynload/:/opt/jumpscale7/bin:/opt/jumpscale7/lib/python.zip:/opt/jumpscale7/lib/plat-x86_64-linux-gnu"
-        echo "[+] Executing testsuite from path : ${testsuite_dir}"
-        cmd="export PYTHONPATH=${python_path}; cd /tmp/G8_testing/functional_testing/Openvcloud; nosetests -s -v ${testsuite_dir} --tc-file config.ini --tc=main.environment:${environment}"
+        echo "[+] Executing testsuite from path : ${testsuite-title}"
+        cmd="export PYTHONPATH=${python_path}; cd ${testsuite_repo_path}/${testsuite-home-dir}; nosetests -s -v ${testsuite-run-dir} --tc-file config.ini --tc=main.environment:${environment}"
         sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "${cmd}"
 
     elif [[ ${action} == "after" ]]; then
 
-        echo "[+] Removing G8_testing repo"
-        sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "rm -rf /tmp/G8_testing"
+        echo "[+] Removing testsuite directory"
+        sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_user}@${ctrl_ipaddress} "rm -rf ${testsuite_repo_path}"
 
     fi
 
