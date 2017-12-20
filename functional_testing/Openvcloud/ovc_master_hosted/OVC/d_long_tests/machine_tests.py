2303dc289e28dae9d5b41ef31f49889cb1e18cc4
Ahmed El-Sayed <ahmed.m.elsayed-93@gmail.com>
2017-12-20 12:07:04 +0200

    fix

--- functional_testing/Openvcloud/ovc_master_hosted/OVC/d_long_tests/machine_tests.py
+++ functional_testing/Openvcloud/ovc_master_hosted/OVC/d_long_tests/machine_tests.py
@@ -158,5 +158,5 @@ class MachineLongTests(BasicACLTest):
         except:
             raise
         finally:
-           response = self.api.scloudbroker.computenode.enable(id=stackId, gid=gridId)
+           response = self.api.cloudbroker.computenode.enable(id=stackId, gid=gridId)
            self.assertTrue(response)
