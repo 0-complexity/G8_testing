ea552e3fcb899619a664592a717701c5363692ef
islamTaha12 <tahai@codescalers.com>
2017-12-27 16:40:03 +0200

    udpate

--- functional_testing/Openvcloud/ovc_master_hosted/OVC/d_long_tests/stress_swap.py
+++ functional_testing/Openvcloud/ovc_master_hosted/OVC/d_long_tests/stress_swap.py
@@ -11,21 +11,37 @@ class StressSwap(BasicACLTest):
 
         #. connect to the node
         #. Install stress-ng
-        #. Run command for 10 minutes
+        #. Run "stress-ng --vm-method rowhammer -r 500"
+        #. Wait for 10 minutes
+        #. Get health detailed status and check the swap value
         #. Make sure that system raise the swap error
         """
-        nodeId = self.get_random_running_nodeId()
-        self.execute_command_on_physical_node('apt-get install -y stress-ng', nodeId)
-        time.sleep(60)
-        self.assertIn('stress-ng', self.execute_command_on_physical_node('which stress-ng', nodeId))
+        self.nodeId = self.get_random_running_self.nodeId()
+        print(" [*] self.nodeId : %s " % str(self.nodeId))
 
-        self.execute_command_on_physical_node('stress-ng --vm-method rowhammer -r 500', nodeId)
+        print(" [*] Install stress-ng")
+        self.execute_command_on_physical_node('apt-get install -y stress-ng', self.nodeId)
+        time.sleep(60)
+        self.assertIn('stress-ng', self.execute_command_on_physical_node('which stress-ng', self.nodeId))
+
+        print(" [*] Execute stress-ng --vm-method rowhammer -r 500")
+        self.execute_command_on_physical_node('stress-ng --vm-method rowhammer -r 500', self.nodeId)
+        print(" [*] Wait for 10 minutes")
         time.sleep(600)
 
-        response_data = self.api.system.health.getDetailedStatus(nid=nodeId)
+        response_data = self.api.system.health.getDetailedStatus(nid=self.nodeId)
+        print(" [*] Get health details status")
         for data in response_data['System Load']['data']:
             if 'Swap' in data['msg']:
                 self.assertEqual("ERROR", data["status"])
                 break
         else:
             self.fail(" [x] There is no Swap sensor!. ")
+
+    def tearDown(self):
+        try:
+            self.execute_command_on_physical_node('pkill -9 stress-ng', self.nodeId)
+        except:
+            # " There is no thing to do, cause this test case may damage the node"
+            pass
+        super(StressSwap, self).tearDown()
