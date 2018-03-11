e05e783aebd0dc90bdeb574f5a63b0cbd6bd3106
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-03-11 11:04:58 +0200

    add disks acl tests

--- functional_testing/Openvcloud/RESTful/testcases/cloudapi/test01_disks.py
+++ functional_testing/Openvcloud/RESTful/testcases/cloudapi/test01_disks.py
@@ -2,8 +2,80 @@ import time, random, unittest
 from testcases import *
 from nose_parameterized import parameterized
 
+class ACLTests(TestcasesBase):
+    def setUp(self):
+        super().setUp()
+        self.admin_api, self.admin = self.api.authenticate_user(groups=['admin'])
+        self.user_api, self.user = self.api.authenticate_user(groups=['user']) 
+        self.CLEANUP['users'].extend([self.admin, self.user])
 
-class DisksTests(TestcasesBase):
+        self.account_id = self.api.create_account()
+        self.assertTrue(self.account_id)
+        self.CLEANUP['accounts'].append(self.account_id)
+        
+        self.disk_data, self.response = self.api.cloudapi.disks.create(accountId=self.account_id, gid=self.gid)
+        self.assertEqual(self.response.status_code, 200, self.response.content)        
+        self.diskId = int(self.response.text)
+
+    def tearDown(self):
+        self.api.cloudapi.disks.delete(diskId=self.diskId)
+        super().tearDown()
+
+    @parameterized.expand([('admin', 200), ('user', 403)])
+    def test01_list_disks(self, role, response_code):
+        """ OVC-001
+        #. Create users (U1) & (U2) with admin access.
+        #. Create user (U3) with user access.
+        #  Create Account (A1) using user (U1).
+        #  Try to list account (A1)'s disks using user (U2), should succeed. 
+        #  Try to list account (A1)'s disks using user (U3), should fail.         
+        """
+        api = self.admin_api if role == 'admin' else self.user_api
+        response = api.cloudapi.disks.list(accountId=self.account_id)
+        self.assertEqual(response.status_code, response_code, response.content)
+
+    @parameterized.expand([('admin', 200), ('user', 403)])
+    def test02_get_disk(self, role, response_code):
+        """ OVC-002
+        #. Create users (U1) & (U2) with admin access.
+        #. Create user (U3) with user access.
+        #  Create Account (A1) using user (U1).
+        #  Try to get disk (D1) using user (U2), should succeed. 
+        #  Try to get disk (D1) using user (U3), should fail.         
+        """
+        api = self.admin_api if role == 'admin' else self.user_api
+        response = api.cloudapi.disks.get(diskId=self.diskId)
+        self.assertEqual(response.status_code, response_code, response.content)
+
+    @parameterized.expand([('admin', 200), ('user', 403)])
+    def test03_resize_disk(self, role, response_code):
+        """ OVC-003
+        #. Create users (U1) & (U2) with admin access.
+        #. Create user (U3) with user access.
+        #  Create Account (A1) using user (U1).
+        #  Try to resize disk (D1) using user (U2), should succeed. 
+        #  Try to resize disk (D1) using user (U3), should fail.         
+        """
+        size = self.disk_data['size'] + random.randint(1, 20)        
+        api = self.admin_api if role == 'admin' else self.user_api
+        response = api.cloudapi.disks.resize(diskId=self.diskId, size=size)
+        self.assertEqual(response.status_code, response_code, response.content)
+
+    @parameterized.expand([('admin', 200), ('user', 403)])
+    def test04_delete_disk(self, role, response_code):
+        """ OVC-004
+        #. Create users (U1) & (U2) with admin access.
+        #. Create user (U3) with user access.
+        #  Create Account (A1) using user (U1).
+        #  Try to delete disk (D1) using user (U2), should succeed. 
+        #  Try to delete disk (D1) using user (U3), should fail.         
+        """
+        api = self.admin_api if role == 'admin' else self.user_api
+        response = api.cloudapi.disks.delete(diskId=self.diskId)
+        self.assertEqual(response.status_code, response_code, response.content)
+
+
+class OperationTests(TestcasesBase):
     def setUp(self):
         super().setUp()
         self.account_id = self.api.create_account()
@@ -116,4 +188,4 @@ class DisksTests(TestcasesBase):
         self.assertEqual(response.status_code, response_code, response.content)
         
 
-    
+
