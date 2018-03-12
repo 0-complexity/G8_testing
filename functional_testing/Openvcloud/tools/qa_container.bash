8e98557ac6b8357674caa1555361c16696c5ee9f
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-03-12 18:15:37 +0200

    add script

---
+++ functional_testing/Openvcloud/tools/qa_container.bash
@@ -0,0 +1,21 @@
+echo "[*] Install zerotier ..."
+curl -s https://install.zerotier.com/ | sudo bash
+
+echo "[*] Starting zerotier service ..."
+zerotier-one -d
+
+echo "[*] Installing pip, pip3 & xvfb ..."
+apt-get install -y python-pip python3-pip xvfb iputils-ping
+
+echo "[*] Add gig user ..."
+echo "gig:x:1000:1000:gig,,,:/home/gig:/bin/bash" >> /etc/passwd && echo gig:rooter | chpasswd
+adduser gig sudo 
+
+echo "[*] Set password to user root ..."
+echo -e "rooter\nrooter" | passwd
+
+echo "[*] Allow password access ..."
+sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+echo "[*] Restart ssh service ..."
+service ssh restart
