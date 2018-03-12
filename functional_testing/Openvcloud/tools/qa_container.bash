736e77a075ad13c1c70ef5924ce4dfcf3da21f4c
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-03-12 18:17:00 +0200

    fix

--- functional_testing/Openvcloud/tools/qa_container.bash
+++ functional_testing/Openvcloud/tools/qa_container.bash
@@ -1,3 +1,5 @@
+password=$1
+
 echo "[*] Install zerotier ..."
 curl -s https://install.zerotier.com/ | sudo bash
 
@@ -8,11 +10,11 @@ echo "[*] Installing pip, pip3 & xvfb ..."
 apt-get install -y python-pip python3-pip xvfb iputils-ping
 
 echo "[*] Add gig user ..."
-echo "gig:x:1000:1000:gig,,,:/home/gig:/bin/bash" >> /etc/passwd && echo gig:rooter | chpasswd
+echo "gig:x:1000:1000:gig,,,:/home/gig:/bin/bash" >> /etc/passwd && echo gig:${password} | chpasswd
 adduser gig sudo 
 
 echo "[*] Set password to user root ..."
-echo -e "rooter\nrooter" | passwd
+echo -e "${password}\n${password}" | passwd
 
 echo "[*] Allow password access ..."
 sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
