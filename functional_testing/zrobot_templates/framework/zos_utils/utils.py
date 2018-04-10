55e865db2eceb529357282749472218a181c0c38
john-kheir <kheirj@codescalers.com>
2018-04-10 13:54:12 +0200

    fixes

--- functional_testing/zrobot_templates/framework/zos_utils/utils.py
+++ functional_testing/zrobot_templates/framework/zos_utils/utils.py
@@ -5,31 +5,31 @@ from js9 import j
 from framework.zos_utils import *
 
 class ZOS_BaseTest(constructor):
-    zos_redisaddr = config['main']['redisAddr']
+    zos_redisaddr = config['main']['redisaddr']
 
     def __init__(self, *args, **kwargs):
         templatespath = ['./framework/zos_utils/templates', './framework/base_templates']
-        super(OVC_BaseTest, self).__init__(templatespath, *args, **kwargs)
-        self.zos_client = self.zos_client(zero_os_ip)
+        super(ZOS_BaseTest, self).__init__(templatespath, *args, **kwargs)
 
     @classmethod
     def setUpClass(cls):
+        cls.zos_client = cls.zos_client(cls, cls.zos_redisaddr)
         cls.zos_node_name = cls.random_string()
-        cls.zos_nodes = {cls.zos_node_name: {redisAddr: zos_redisaddr,
-                                            redisPort: 6379, hostname: "myzeros"}}
+        cls.zos_nodes = {cls.zos_node_name: {'redisAddr': cls.zos_redisaddr,
+                                             'redisPort': 6379, 'hostname': "myzeros"}}
         cls.cont_flist = 'https://hub.gig.tech/gig-official-apps/ubuntu1604.flist'
         cls.cont_storage = 'ardb://hub.gig.tech:16379'
 
     @classmethod
-    def tearDownClass(self):
-        self.delete_services()
+    def tearDownClass(cls):
+        cls.delete_services(cls())
 
     def setUp(self):
-        super(OVC_BaseTest, self).setUp()
+        super(ZOS_BaseTest, self).setUp()
 
     def zos_client(self, ip):
-        data = {host: ip, port:6379,
-                timeout=100, ssl: True}
+        data = {'host': ip, 'port':6379,
+                'timeout': 100, 'ssl': True}
         return j.clients.zero_os.get(instance='main', data=data)
 
     def handle_blueprint(self, yaml, **kwargs):
