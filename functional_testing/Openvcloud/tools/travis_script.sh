39a35ef8b23c801d5585078562a0edfc4b8d04f0
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-05-14 12:51:51 +0200

    init commit

--- functional_testing/Openvcloud/tools/travis_script.sh
+++ functional_testing/Openvcloud/tools/travis_script.sh
@@ -3,11 +3,25 @@ working_path="/tmp/travis_${TRAVIS_BUILD_NUMBER}"
 python_path='/opt/jumpscale7/lib:/opt/jumpscale7/lib/lib-dynload/:/opt/jumpscale7/bin:/opt/jumpscale7/lib/python.zip:/opt/jumpscale7/lib/plat-x86_64-linux-gnu'
 
 execute(){
-    sshpass -p ${jumpserver_password} ssh -o StrictHostKeyChecking=no -t -l ${jumpserver_username} ${jumpserver_ipaddress} \
-    sshpass -p ${ctrl_password} ssh -o StrictHostKeyChecking=no -t -l ${ctrl_username} ${ctrl_ipaddress} ${1}
+    sshpass -p ${ctrl_password} ssh -t -o StrictHostKeyChecking=no ${ctrl_username}@${ctrl_ipaddress} ${1}
+}
+
+execute_as_root(){
+    sshpass -p ${ctrl_root_password} ssh -t -o StrictHostKeyChecking=no root@${ctrl_ipaddress} ${1}
+}
+
+join_zerotier_network(){
+    echo "[+] Joining zerotier network: ${1}"
+    sudo zerotier-cli join ${1}; sleep 5
+    memberid=$(sudo zerotier-cli info | awk '{print $3}')
+    curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${2}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${1}/member/${memberid} > /dev/null
+    # fix for travis - zerotier issue 
+    sudo ifconfig "$(ls /sys/class/net | grep zt)" mtu 1280 
 }
 
 if [[ ${action} == "setup" ]]; then
+
+    join_zerotier_network ${zerotier_network} ${zerotier_token}
 
     execute "mkdir -p ${working_path}"
     execute "git clone -b ${TRAVIS_BRANCH} https://github.com/0-complexity/G8_testing ${working_path}/G8_testing"
@@ -23,10 +37,10 @@ elif [[ ${action} == "acl" ]] && echo "${jobs}" | grep -q "acl"; then
 elif [[ ${action} == "ovc" ]] && echo "${jobs}" | grep -q "ovc"; then
 
     cmd="export PYTHONPATH=${python_path} \; cd ${working_path}/G8_testing/functional_testing/Openvcloud \; nosetests-2.7 -s -v ${ovc_testsuite_dir} --tc-file config.ini --tc=main.email:${test_email} --tc=main.email_password:${test_email_password} --tc=main.environment:${environment}"
-    execute "${cmd}"
+    execute_as_root "${cmd}"
 
 elif [[ ${action} == "portal" ]] && echo "${jobs}" | grep -q "portal"; then
-    ctrl_username="gig"
+    
     cmd="cd ${working_path}/G8_testing \; bash functional_testing/Openvcloud/ovc_master_hosted/Portal/travis_portal_script.sh ${environment} ${portal_admin} ${portal_password} ${portal_secret} ${portal_testsuite_dir} ${portal_browser} ${ctrl_password}"
     execute "${cmd}"
 
