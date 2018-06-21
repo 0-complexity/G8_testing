2cd3100028148d14f83741c9d4db8ba87618a4b9
islamtaha <itaha@greenitglobe.com>
2018-06-21 13:51:47 +0200

    fix1

--- functional_testing/primitives/main.py
+++ functional_testing/primitives/main.py
@@ -20,7 +20,7 @@ parser.add_argument("-n", dest="zos_nodes", type=int, default=1, help="no. of zo
 parser.add_argument("-k", dest="ubuntu_vms", type=int, default=1, help="no. of ubuntu vms, default 1")
 args = parser.parse_args()
 
-NUMBER_OF_ZOS_NODES = args.search_query
+NUMBER_OF_ZOS_NODES = args.zos_nodes
 NUMBER_OF_UBUNTU_VMS = args.ubuntu_vms
 
 SSHKEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPfKUsaFuaGJsnHvF3k0PbqQTr3GL2pNuddn/xQjsroF35ELJVEovAsd9IlsFWOmDWlL6B+JYFgj8g5IykklHCDfmTu6LcGXjdfAYVp+eXARmgoCJKxVyenSVHu6No9O1e+QKFvMJTiJXdl08fZD1Fd2kRetDRKAijCZ76pmB4/KwVFiJKCVVdsDW/0R+td0gNVJyCQyRTcWEPmBfGMW/JrvRCSHfxlLdqsD3txLOm9pHlQ/LmEwOP3bqEEpQU1jP32JbdAdreuD6BYB+YRp02yyU33gd1QbqIEgftcN+6TuZJOU3j2VRSiUQX8h5SjtWV1UXE15ELlIlhcFJYH6L root@islamtaha-TT'
