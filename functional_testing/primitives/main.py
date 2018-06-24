92940219fc587c029915e0199da0efe66e58f56e
islamtaha <itaha@greenitglobe.com>
2018-06-24 12:17:31 +0200

    primitives

--- functional_testing/primitives/main.py
+++ functional_testing/primitives/main.py
@@ -24,10 +24,10 @@ NUMBER_OF_ZOS_NODES = args.zos_nodes
 NUMBER_OF_UBUNTU_VMS = args.ubuntu_vms
 
 SSHKEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjPfKUsaFuaGJsnHvF3k0PbqQTr3GL2pNuddn/xQjsroF35ELJVEovAsd9IlsFWOmDWlL6B+JYFgj8g5IykklHCDfmTu6LcGXjdfAYVp+eXARmgoCJKxVyenSVHu6No9O1e+QKFvMJTiJXdl08fZD1Fd2kRetDRKAijCZ76pmB4/KwVFiJKCVVdsDW/0R+td0gNVJyCQyRTcWEPmBfGMW/JrvRCSHfxlLdqsD3txLOm9pHlQ/LmEwOP3bqEEpQU1jP32JbdAdreuD6BYB+YRp02yyU33gd1QbqIEgftcN+6TuZJOU3j2VRSiUQX8h5SjtWV1UXE15ELlIlhcFJYH6L root@islamtaha-TT'
-IYO_CLIENT_ID = 'wrzcIurg4zXEjlJOA1z0CX806jKx'
-IYO_CLIENT_SECRET = 'WaKRgZgLTHdTfFEgkrSfieKkd734'
-PACKET_AUTH = 'vqzwphNXwsr7LrBCAEX87iaqSbSBWxeZ'
-ZT_TOKEN = 'MyuDhfUYxCkc8eCH8cA4bt4WMgR6saWT'
+IYO_CLIENT_ID = ''
+IYO_CLIENT_SECRET = ''
+PACKET_AUTH = ''
+ZT_TOKEN = ''
 OVC_DATA = {"address": "be-g8-3.demo.greenitglobe.com",
             "location": "be-g8-3",
             "port": 443}
